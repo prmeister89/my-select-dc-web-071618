6f42ff79aac5f7f5170d85577fe7674a1076cd9c
Peter Meister <prmeister89@gmail.com>
2018-07-09 23:34:58 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
+require "pry"
 def my_select(collection)
- # your code here!
+  i = 0
+  new_array = []
+  
+  while i < collection.length
+    if yield(collection[i]) == true
+      new_array << collection[i]
+    end
+    i+=1
+  end
+  return new_array
 end
